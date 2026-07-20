4426539573cbe568b55a8398513a9690ce6bc3c3
Quinntyne Brown <quinntynebrown@gmail.com>
2026-07-20 03:36:34 -0400

    Remove Cornerstone library project

--- projects/cornerstone/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/** Public API for @faithtech/cornerstone. */
-export * from './lib/primitives';
-export * from './lib/forms';
-export * from './lib/disclosure';
-export * from './lib/navigation';
-export * from './lib/overlays';
